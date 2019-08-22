a4b4236f595f8c3cfc1392bf5046bf71e086d4e1
heperwong <41944890+heperwong@users.noreply.github.com>
2019-08-22 18:21:53 +0800

    syntax数据库改成toc数据库集名

--- sql/sql.js
+++ sql/sql.js
@@ -2,7 +2,7 @@
 module.exports = {
     queryList: 'SELECT * FROM [macro].[dbo].[toc$]',
     queryNameDetail: "SELECT * FROM [macro].[dbo].[toc$] where MacroName = ",
-    querySyntax: "SELECT * FROM [macro].[dbo].[syntax$] where MacroName = ",
+    querySyntax: "SELECT * FROM [macro].[dbo].[toc$] where MacroName = ",
     queryParam: "SELECT * FROM [macro].[dbo].[param$] where MacroName = ",
     del: 'DELETE FROM ?? WHERE id=?',
 };
