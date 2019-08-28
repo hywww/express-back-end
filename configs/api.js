fb4f1e5a3376538a309eecfc55fa36c962784ea9
heperwong <41944890+heperwong@users.noreply.github.com>
2019-08-28 17:07:01 +0800

    增加getHistoryVersionCode

--- configs/api.js
+++ configs/api.js
@@ -6,6 +6,8 @@ module.exports = {
     getDetail: path + '/get/list/detail',
     getSyntax: path + '/get/list/syntax',
     getParam: path + '/get/list/param',
+    getHistory: path + '/get/list/history',
+    getHistoryVersionCode: path + '/get/history/detail',
     getSourceCode: path + '/get/sourceCode',
     saveFeedBack: path + '/save/feedback/item',
     feedBackList: path + '/get/feedback/list',
