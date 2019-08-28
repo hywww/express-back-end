9d28b872e20b12e785f5f024fa4f30bda226c5ac
heperwong <41944890+heperwong@users.noreply.github.com>
2019-08-28 11:23:58 +0800

    读取history库

--- sql/sql.js
+++ sql/sql.js
@@ -4,5 +4,6 @@ module.exports = {
     queryNameDetail: "SELECT * FROM [macro].[dbo].[toc$] where MacroName = ",
     querySyntax: "SELECT * FROM [macro].[dbo].[toc$] where MacroName = ",
     queryParam: "SELECT * FROM [macro].[dbo].[param$] where MacroName = ",
+    history:"SELECT * FROM [macro].[dbo].[history_$] where MacroName = ",
     del: 'DELETE FROM ?? WHERE id=?',
 };
